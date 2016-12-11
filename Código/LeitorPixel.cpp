38b0f04106f4574cb157195de599998c2a2f2d7c
Eu <hsufhiusfdhisdfuhisfudhisfud@fdofsdhoshgjhashdoiahdoiadshoh.com>
2016-12-11 00:42:00 -0200

    Versão 1.2

--- Código/LeitorPixel.cpp
+++ Código/LeitorPixel.cpp
@@ -131,6 +131,79 @@ int *geraArquivoAdptado( string nomeArquivo, int *tamanhoVetor )
 	return vetorPixels;
 }
 
+int *geraVetorImagemPorLinha( string nomeArquivo, int *tamanhoVetor, char **nomeArquivoSaida, ifstream &arquivoEntrada )
+{
+	vector<int> pixels;
+	string nome = nomeArquivo;
+	string linha;
+	string aditivoNomeLinha;
+	char *token;
+	char *tokenAditivo;
+	int tamanho;
+
+	//cout << "Nome do Arquivo: " << nome << endl;
+	
+	/*ifstream arquivoEntrada( nome.c_str(), ios::in );
+	if ( !arquivoEntrada )
+	{
+		cerr << "Erro ao abrir o arquivo de entrada \"" << nome << "\". Encerrando o programa." << endl;
+		exit( 0 );
+	}*/
+
+	getline( arquivoEntrada, linha );
+	if ( strcmp( linha.c_str(), "\n" ) == 0 || strcmp( linha.c_str(), "" ) == 0 )
+		return NULL;
+	//cout << "Linha" << endl << linha << endl << "Fim Linha" << endl;
+	tamanho = strlen( linha.c_str() ) + 1;
+	//char splitString[ tamanho ];
+
+	char *splitString = (char *)malloc(sizeof(char) * tamanho );
+	if ( splitString == NULL )
+	{
+		cerr << "Erro ao alocar splitString. Falta de memória" << endl;
+		exit( 0 );
+	}
+
+
+	strcpy( splitString, linha.c_str() );
+
+	aditivoNomeLinha = tokenAditivo = strtok( splitString , "," );
+	token = strtok( NULL, "," );
+	if ( tokenAditivo == NULL || token == NULL )
+	{
+		cerr << "A linha do arquivo entrada está inconsistente. Por-favor, verifique se o arquivo de entrada segue o padrão correto." << endl;
+		exit( 0 );
+	}
+
+
+	strcpy( splitString, token );
+	token = strtok( splitString, " " );
+	// Leitura de pixels
+	while ( token != NULL )
+	{
+		//cout << token << endl;
+		pixels.push_back( atoi( token ) );
+		//cout << pixels.back() << endl;
+		token = strtok( NULL, " " );
+	}
+
+	//cout << endl;
+	int *vetorPixels = converteVectorParaArray( pixels );
+	
+	free( splitString );
+
+	//escreveArquivoSaida( nome, aditivoNomeLinha, vetorPixels, pixels.size() );
+	(*nomeArquivoSaida) = (char *)malloc(sizeof( char ) * ( strlen(aditivoNomeLinha.c_str()) + 1 ) );
+	if ( (*nomeArquivoSaida) == NULL )
+	{
+		cerr << "Erro ao alocar nome do arquivo saida." << endl;
+		exit( 0 );
+	}
+	strcpy( (*nomeArquivoSaida), aditivoNomeLinha.c_str() );
+	*tamanhoVetor = pixels.size();
+	return vetorPixels;
+}
+
 /* PARA TESTES E EXEMPLO DE USO */
 /*int main()
 {
--- Código/LeitorPixel.h
+++ Código/LeitorPixel.h
@@ -1,6 +1,13 @@
 #ifndef LEITORPIXEL_H
 #define LEITORPIXEL_H
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
 
 int *geraArquivoAdptado( string nomeArquivo, int *tamanhoVetor );
+
+int *geraVetorImagemPorLinha( string nomeArquivo, int *tamanhoVetor, char **nomeArquivoSaida, ifstream &arquivoEntrada );
 
 #endif
